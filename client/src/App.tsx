18152ce414bed65b50caeb6084d5d1bed305b405
Flinn <358530466@qq.com>
2019-10-20 21:40:21 +0800

    layout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import Layout from './pages/Layout'
+import _Layout from './pages/Layout'
 import { BrowserRouter, Route } from 'react-router-dom'
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
-      <Route path='/' component={Layout}></Route>
+      <Route path='/' component={_Layout}></Route>
     </BrowserRouter>
   )
 }
